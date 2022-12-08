5f69a0232e565c89f74cd20f9c3b711c08524636
Cyrus Jackson <cyjackso@microsoft.com>
2022-12-08 15:43:40 +0530

    Fix test

--- src/tests/ignore_functions_test/ignore_functions_test.cpp
+++ src/tests/ignore_functions_test/ignore_functions_test.cpp
@@ -11,9 +11,15 @@ std::string GetOSVersion() {
     return osVersion;
 }
 
+std::string SomeOtherString() {
+    std::string osVersion = "Windows";
+    return osVersion;
+}
+
 int main(int argc, char** argv)
 {
     static std::string const osVer = GetOSVersion();
+    static std::string const someOtherString = SomeOtherString();
 
     int leaks = static_cast<int>(VLDGetLeaksCount());
     if (0 != leaks)
