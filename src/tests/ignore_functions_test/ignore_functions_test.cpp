4284393133b1fc30c4ddead897cee8d30e2c156c
Cyrus Jackson <cyjackso@microsoft.com>
2023-01-11 21:45:19 +0530

    Use native Set for ignore functions

--- src/tests/ignore_functions_test/ignore_functions_test.cpp
+++ src/tests/ignore_functions_test/ignore_functions_test.cpp
@@ -16,10 +16,22 @@ std::string SomeOtherString() {
     return osVersion;
 }
 
+std::string abcdefg() {
+    std::string osVersion = "Windows";
+    return osVersion;
+}
+
+std::string testOtherString() {
+    std::string osVersion = "Windows";
+    return osVersion;
+}
+
 int main(int argc, char** argv)
 {
     static std::string const osVer = GetOSVersion();
     static std::string const someOtherString = SomeOtherString();
+    static std::string const str3 = abcdefg();
+    static std::string const str4 = testOtherString();
 
     int leaks = static_cast<int>(VLDGetLeaksCount());
     if (0 != leaks)
